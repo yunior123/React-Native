0b7b2cefd54a83fcce3229182aa7a61a2915a580
yunior123 <yuniorrodriguezo460@gmail.com>
2024-05-20 08:01:56 -0400

    hj

--- FirstProject/app/(tabs)/index.tsx
+++ FirstProject/app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
+import { View} from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { LittleLemonHeader } from '@/components/LittleLemonHeader';
 
 export default function HomeScreen() {
+  return (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#495E57',
+          }}>
+          <LittleLemonHeader />
+        </View>
+      );
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
